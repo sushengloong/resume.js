29696faecd39402f3e066762e3b1aedece817245
Sheng-Loong Su <sushengloong@gmail.com>
2019-11-02 17:12:30 -0700

    add boostrap

---
+++ src/components/resume/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <main role="main" class="container">
+      <div class="starter-template">
+        <h1>Resume skeleton</h1>
+        <p class="lead">
+          The world's best resume!
+        </p>
+      </div>
+    </main>
+  );
+}
